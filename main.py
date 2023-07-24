7426956e0b0d7bdc125967306427a27593833d75
Gal Jeza <61537604+galjeza@users.noreply.github.com>
2023-07-24 19:49:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -395,7 +395,7 @@ def main():
     chrome_options = Options()
 
     #chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(ChromeDriverManager(version="114.0.5735.16").install(), options=chrome_options)
     print("=> vsi gonilniki uspešno pridobljeni")
 
     driver.get("https://www.avto.net/_2016mojavtonet/")
